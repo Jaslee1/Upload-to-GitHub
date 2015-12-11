fc1a119453c6f1e7d72305865d83a4df9bdba47d
Jaslee1 <jasleedesign@gmail.com>
2015-12-11 17:50:12 -0500

    Tech273 Project

---
+++ final project1/nav_menu.js
@@ -0,0 +1,11 @@
+/*global $ */
+
+$(document).ready(function () {
+    "use strict";
+    $("#navigation").navPlugin({
+        'itemWidth': 105,
+        'itemHeight': 35,
+        'navEffect': "fade",
+        'speed': 500
+    });
+});
